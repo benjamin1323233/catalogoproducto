79b28aea82618d99e190bf2982e1f88ca5b89663
Quiros Benjamin <bq170921@gmail.com>
2025-07-07 17:11:43 -0300

    realice la logica del login

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import Administrador from "./components/pages/Administrador";
 import DetalleProducto from "./components/pages/DetalleProducto";
 import Error404 from "./components/pages/Error404";
 import Inicio from "./components/pages/Inicio";
+import Login from "./components/pages/Login";
 import CardProducto from "./components/pages/producto/CardProducto";
 import { Route,Routes,BrowserRouter } from "react-router";
 import FormularioProducto from "./components/pages/producto/FormularioProducto";
 import Footer from "./components/shared/Footer";
 import Menu from "./components/shared/Menu";
-import Login from "./components/pages/Login";
+import { useState } from "react";
 
 function App() {
+  //no hace falta json poruqe no es un objeto
+  const usuariologueado = sessionStorage.getItem("userkey")||false 
+  const [usuarioAdmin, setUsuarioadmin]=useState(usuariologueado)
   return (
     <>
  <BrowserRouter>
@@ -18,7 +22,7 @@ function App() {
 <Routes>
   <Route path="/" element={<Inicio></Inicio>}></Route>
   <Route path="/detalle" element={<DetalleProducto></DetalleProducto>}></Route>
-  <Route path="/login" element={<Login></Login>} ></Route>
+  <Route path="/login" element={<Login setUsuarioadmin={setUsuarioadmin} ></Login>} ></Route>
   <Route path="/admin" element={<Administrador></Administrador>}></Route>
   <Route path="/admin/crear" element={<FormularioProducto></FormularioProducto>}></Route>
   <Route path="/admin/editar" element={<FormularioProducto></FormularioProducto>}></Route>
--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,27 +1,67 @@
 import { Row,Col } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
 
-const Login = () => {
+const Login = ({setUsuarioadmin}) => {
+
+  //esto es de react hook form
+const{
+  register,
+  handleSubmit,
+  formState:{ errors },
+}=useForm()
+
+const navegacion = useNavigate()
+
+const iniciarSesion=(usuario)=>{
+    console.log(import.meta.env.VITE_API_EMAIL)
+    if(usuario.email=== import.meta.env.VITE_API_EMAIL &&
+      usuario.password=== import.meta.env.VITE_API_PASSWORD){
+      console.log("soy el administrador")
+      setUsuarioadmin(true)
+      sessionStorage.setItem("userkey", true)
+      //redirige a otra pdina
+      navegacion("/admin")
+      }else{
+        console.log("email o contraseña no son correctas")
+      }
+}
+
     return (
         <section className="container my-3">
               <h1 className="text-center">Login</h1>  
             <Row xs={1} md={2}>
                 <Col>
-      <Form>
+      <Form onSubmit={handleSubmit(iniciarSesion)}>
          <Form.Group className="mb-3" controlId="email">
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="ingresa un email" />
+        <Form.Control type="email" placeholder="ingresa un email" {...register("email",{
+          required:"el email es un dato obligatorio",
+          pattern:{
+value:/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+message:"EL EMAIL  DEBE TENER UN FORMATO VALIDO"
+
+          }
+
+        })} />
         <Form.Text className="text-danger">
-este es un mensaje de error
+          {errors.email?.message}
         </Form.Text>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="password">
         <Form.Label>contraseña</Form.Label>
-        <Form.Control type="password" placeholder="ingresa tu contraseña" />
+        <Form.Control type="password" placeholder="ingresa tu contraseña"{...register("password",{
+          required:"la contraseña es un dato obligatorio",
+          pattern:{
+            value:/^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/,
+            message:"La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter no alfanumérico."
+          }
+        })} />
         <Form.Text className="text-danger">
-este es un mensaje de error
+{errors.password?.message}
         </Form.Text>
       </Form.Group>
       <Button variant="success" type="submit">
